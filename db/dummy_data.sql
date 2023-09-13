1bb37dc6041c9fa3b88729309df552bb19c9cd55
Danny Ho <113899395+DannyHo1209@users.noreply.github.com>
2023-09-13 22:40:59 +0800

    fixed the syntax error of dummy data generator

--- db/dummy_data.sql
+++ db/dummy_data.sql
@@ -12,9 +12,9 @@ INSERT INTO Item (item_name, item_description, item_serial_number, item_status, 
   ("CalicoCat", "Calico cat 1.", "C002", 0, 2, 2),
   ("Border Collie1", "A dog.", "D001", 0, 1, 3),
   ("Border Collie2", "Another dog.", "D002", 0, 1, 4),
-  ("Gray cat", "A cute gray cat.". "C003", 0, 2, 5);
+  ("Gray cat", "A cute gray cat.", "C003", 0, 2, 5);
 
-INSERT INTO RentalForm (renter_name, renter_student_id, renter_phone_number, renter_contact_info, rental_note, rental_lend_date, rental_due_date, rental_rent, rental_rent_pay_date, rental_item_return_date, rental_item_id) VALUES
+INSERT INTO RentalForm (renter_name, renter_student_id, renter_phone_number, renter_contact_info, rental_note, rental_item_lend_date, rental_item_due_date, rental_rent, rental_rent_pay_date, rental_item_return_date, rental_item_id) VALUES
   ("林周罵", "t100010101", "0900000000", "LineID: DontContactMe", "借來玩玩", "2022-09-11", "2022-09-12", 500, "2022-09-11", "2022-09-12", 1),
   ("林周罵", "t100010101", "0900000000", "LineID: DontContactMe", "", "2022-09-15", "2022-09-23", 500, "2022-09-15", "2022-09-23", 4),
   ("傅榆儒", "t012949182", "0912348198", "Email: haha@gmail.com", "", "2022-10-01", "2022-10-09", 500, "2022-10-01", "2022-10-09", 2),
@@ -27,5 +27,5 @@ INSERT INTO RentalForm (renter_name, renter_student_id, renter_phone_number, ren
   ("金珽昀", "t273569129", "0978593123", "Facebook: WhyRURunning", "旅遊", "2023-02-10", "2023-02-16", 500, "2023-02-13", "2023-02-16", 3),
   ("段萬謙", "t821903484", "0912321491", "", "", "2023-04-01", "2023-04-02", 500, "2023-04-01", "2023-04-02", 2);
 
-INSERT INTO AbandonedRentalForm (renter_name, renter_student_id, renter_phone_number, renter_contact_info, rental_note, rental_lend_date, rental_due_date, rental_rent, rental_rent_pay_date, rental_item_return_date, rental_item_id) VALUES
-  ("林周罵", "t100010101", "0900000000", "LineID: DontContactMe", "借來玩玩", "2022-09-13", "2022-09-14", 500, "2022-09-13", "", 1),
+INSERT INTO AbandonedRentalForm (renter_name, renter_student_id, renter_phone_number, renter_contact_info, rental_note, rental_item_lend_date, rental_item_due_date, rental_rent, rental_rent_pay_date, rental_item_return_date, rental_item_id) VALUES
+  ("林周罵", "t100010101", "0900000000", "LineID: DontContactMe", "借來玩玩", "2022-09-13", "2022-09-14", 500, "2022-09-13", "0000-00-00", 1);
